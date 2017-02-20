4c309effa0fba9d55d506a7e40797e726817317c
partrick.li <partrick.li@gmail.com>
2017-02-20 17:01:07 +0800

    FacialExpression added

---
+++ FaceIt/FacialExpression.swift
@@ -0,0 +1,50 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by liguiyan on 2017/2/20.
+//  Copyright © 2017年 partrick. All rights reserved.
+//
+
+import Foundation
+
+struct FacialExpression {
+    
+	enum Eyes: Int {
+		case open
+		case closed
+		case squinting
+	}
+
+	enum EyeBrows: Int {
+		case relaxed
+		case normal
+		case furrowed
+		
+		var moreRelaxed: EyeBrows {
+			return EyeBrows(rawValue: rawValue - 1) ?? .relaxed
+		}
+		var moreFurrowed: EyeBrows {
+			return EyeBrows(rawValue: rawValue + 1) ?? .furrowed
+		}
+	}
+
+	enum Mouth: Int {
+		case frown
+		case smirk
+		case neutral
+		case grin
+		case smile
+
+		var happier: Mouth {
+			return Mouth(rawValue: rawValue + 1) ?? .smile
+		}
+		var sadder: Mouth {
+			return Mouth(rawValue: rawValue - 1) ?? .frown
+		}
+	}
+	
+	var eye = Eyes.open
+	var eyeBrows = EyeBrows.relaxed
+	var mouth = Mouth.smile
+}
