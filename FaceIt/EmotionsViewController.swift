bb1c0ff31ea8ede21e0443f62ee7722eeac63aca
partrick.li <partrick.li@gmail.com>
2017-02-23 18:35:50 +0800

    emotion view controller added

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by liguiyan on 2017/2/23.
+//  Copyright © 2017年 partrick. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+
+}
