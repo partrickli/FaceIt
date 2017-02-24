7de6647826543f419a94e75c9a9f4f1e30302aba
partrick.li <partrick.li@gmail.com>
2017-02-24 13:57:34 +0800

    segue demo lecture 5 completed

--- FaceIt/EmotionsViewController.swift
+++ FaceIt/EmotionsViewController.swift
@@ -18,11 +18,18 @@ class EmotionsViewController: UIViewController {
     ]
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let faceVC = segue.destination as? FaceViewController else {
-            return
+        var destinationVC = segue.destination
+        if let navigationVC = destinationVC as? UINavigationController {
+            destinationVC = navigationVC.visibleViewController ?? destinationVC
         }
-        if let segueIdentifier = segue.identifier, let expression = facialExpressioins[segueIdentifier] {
+        if let faceVC = destinationVC as? FaceViewController,
+            let segueIdentifier = segue.identifier,
+            let expression = facialExpressioins[segueIdentifier] {
+            
             faceVC.expression = expression
+            if let button = sender as? UIButton {
+                faceVC.navigationItem.title = button.currentTitle
+            }
         }
     }
 }
